8c2dd3c29cce12d5ba59d32114c445f975b465f1
SBMOYO <sunganomoyo1@gmail.com>
2023-05-05 08:48:12 +0200

    admin page

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 
 app.secret_key = 'my_secret_key'
 app.config['SESSION_COOKIE_NAME'] = 'online_shoe_store'
+app.config['IMAGE_UPLOADS'] = 'static/assets/uploads'
 
 #db = sqlite3.connect("main.db")
 
@@ -41,7 +42,13 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    # Query database for username
+    rows = db.execute("SELECT * FROM shoe_stock;")
+
+
+
+    return render_template("index.html", rows=rows)
 
 
 @app.route("/logIn", methods = ["GET", "POST"])
@@ -54,7 +61,7 @@ def log_in():
         password = request.form.get("password")
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE email = ?;", username)
+        rows = db.execute("SELECT * FROM users WHERE username = ?;", username)
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["password"], password):
@@ -103,3 +110,47 @@ def register():
         return render_template("register.html")
 
 
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+
+    if request.method == "POST":
+
+        shoe_name = request.form.get("shoe_name")
+        quantity = request.form.get("quantity")
+        shoe_size = request.form.get("shoe_size")
+        shoe_colour = request.form.get("shoe_colour")
+
+        if request.files:
+            image = request.files.get("image")
+            
+            image.save(os.path.join(app.config['IMAGE_UPLOADS'], image.filename))
+  
+        # Create a table
+        db.execute("""
+            CREATE TABLE IF NOT EXISTS shoe_stock (
+            shoe_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            shoe_name TEXT NOT NULL,
+            quantity INTEGER NOT NULL,
+            shoe_size INTEGER NOT NULL,
+            shoe_colour INTEGER NOT NULL,
+            shoe_image TEXT NOT NULL
+            )
+        """)
+
+          # Query database for username
+        rows = db.execute("SELECT * FROM shoe_stock WHERE shoe_name = ?;", shoe_name)
+
+         # Ensure the username name does not already exist
+        if len(rows) != 0:
+
+            # Insert shoe details into database
+            id = db.execute("UPDATE shoe_stock SET quantity = ?, shoe_size = ?, shoe_colour = ? WHERE shoe_name = shoe_name",
+                            quantity, shoe_size, shoe_colour)
+        # Insert shoe details into database
+        id = db.execute("INSERT INTO shoe_stock (shoe_name, quantity, shoe_size, shoe_colour, shoe_image) VALUES (?, ?, ?, ?, ?);",
+                        shoe_name, quantity, shoe_size, shoe_colour, image.filename)
+        
+        return redirect("/")
+        
+
+    return render_template("upload.html")
